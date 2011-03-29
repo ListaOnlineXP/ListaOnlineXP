e6e7c88b5450deed2a02af7dfbcda9f94abcd0ee
Hugo Soares Zaggo <hugozaggo@gmail.com>
2011-03-29 16:14:05 -0300

    Exercise lists created, questions created. We still need to come up with a elegant solution for the relationship between questions and answers.

--- src/ListaOnline/course/admin.py
+++ src/ListaOnline/course/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from ListaOnline.course.models import Course, Teacher
+from ListaOnline.course.models import Course, Teacher, ExerciseList, Question, Answer
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'teacher')
     
+class ExerciseListAdmin(admin.ModelAdmin):
+    filter_horizontal = ('question',)
+    
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Teacher)
+admin.site.register(ExerciseList, ExerciseListAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
--- src/ListaOnline/course/models.py
+++ src/ListaOnline/course/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
@@ -14,6 +15,30 @@ class Course(models.Model):
     
     def __unicode__(self):
         return self.name
-        
 
+class Question(models.Model):
+
+    text = models.TextField(blank=False)
+
+    def __unicode__(self):
+        return self.text
+        
+class Answer(models.Model):
+
+    text = models.TextField(blank=False)
+    question = models.ForeignKey(Question, related_name="question")
+    answer = models.OneToOneField(Question, related_name="answer")
+
+    def __unicode__(self):
+        return self.text        
+
+class ExerciseList(models.Model):
     
+    name = models.CharField(blank=False, max_length=100)
+    course = models.ForeignKey(Course)
+    pub_date = models.DateField(default=datetime.datetime.today)
+    due_date = models.DateField(default=(datetime.datetime.today()+datetime.timedelta(days=7)))
+    question = models.ManyToManyField(Question)
+    
+    def __unicode__(self):
+        return self.name
