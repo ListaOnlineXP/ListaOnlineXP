da180a7b393e8d91a56f6c717f543f086d3dd38a
ricardo juliano mesquita silva oda <ricardo.oda@vigarista.cec.ime.usp.br>
2011-03-30 16:20:04 -0300

    criado testes iniciais

--- src/ListaOnline/course/tests.py
+++ src/ListaOnline/course/tests.py
@@ -1,23 +1,56 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from django.test import TestCase
+from ListaOnline.course.models import Course
+from ListaOnline.course.models import Teacher
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class TeacherTestCase(TestCase):
+    
+    def setUp(self):
+        self.teacher1 = Teacher.objects.create(name="Alfredo")
+        self.teacher2 = Teacher.objects.create(name="Gerosa")
+        self.teacher3 = Teacher.objects.create(name="Flávio")
+        
+    def testTeacherDB(self):
+        self.assertEqual(Teacher.objects.get(name="Alfredo").name, "Alfredo")
+        self.assertEqual(Teacher.objects.get(name="Gerosa").id, 2)
+        self.assertNotEqual(Teacher.objects.get(id="3").name,"Alfredo")
+        self.assertNotEqual(Teacher.objects.get(id="3").name,"Gerosa")
+        self.assertEqual(Teacher.objects.count(), 3)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class CourseTestCase(TestCase):
+    
+    def setUp(self):
+        self.teacher1 = Teacher.objects.create(name="Alfredo")
+        self.teacher2 = Teacher.objects.create(name="Gerosa")
+        self.course1 = Course.objects.create(code="MAC0110", name="Introdução à Computação", teacher=Teacher.objects.get(id=1))
+        self.course2 = Course.objects.create(code="MAC0122", name="Algoritmos", teacher=Teacher.objects.get(id=2))
 
->>> 1 + 1 == 2
-True
-"""}
-
+    def testExist(self):
+        self.assertEqual(Course.objects.get(id=1).code, "MAC0110")
+        self.assertEqual(Course.objects.get(teacher="Alfredo").code, "MAC0110")
+        self.assertEqual(Course.objects.get(teacher="Alfredo").id, 1)
+        self.assertEqual(Course.objects.get(id=1).name, "Introdução à Computação")
+        self.assertEqual(Course.objects.get(teacher="Alfredo").name, "Introdução à Computação")
+        self.assertNotEqual(Course.objects.get(id=1).teacher, Teacher.objects.get(name="Gerosa"))
+        self.assertEqual(Course.objects.get(id=1).teacher, Teacher.objects.get(name="Alfredo"))
+        self.assertEqual(Course.objects.get(id=2).code, "MAC0122")
+        self.assertEqual(Course.objects.get(teacher="Gerosa").code, "MAC0122")
+        self.assertEqual(Course.objects.get(id=2).name, "Algoritmos")
+        self.assertEqual(Course.objects.get(teacher="Gerosa").name, "Algoritmos")
+        self.assertNotEqual(Course.objects.get(id=2).teacher, Teacher.objects.get(name="Alfredo"))
+        self.assertEqual(Course.objects.get(id=2).teacher, Teacher.objects.get(name="Gerosa"))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
