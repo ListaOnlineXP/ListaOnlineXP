c01b840a65fb80973715ded81c21549c6f36ba65
ricardo juliano mesquita silva oda <ricardo.oda@vigarista.cec.ime.usp.br>
2011-04-15 15:14:25 -0300

    some refactoring

--- ListaOnlineXP/course/forms.py
+++ ListaOnlineXP/course/forms.py
@@ -4,36 +4,36 @@ from django import forms
 
 class SignUpForm(forms.Form):
 
-	name = forms.CharField(max_length=100)
-	username = forms.CharField(max_length=20, label='Nome de usuário')
-	nusp = forms.CharField(max_length=100)
-	passwd = forms.CharField(max_length=20, widget=forms.PasswordInput)
-	confirm_passwd = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    name = forms.CharField(max_length=100, label=u'Nome')
+    username = forms.CharField(max_length=20, label=u'Nome de usuário')
+    nusp = forms.CharField(max_length=100, label=u'Número USP')
+    passwd = forms.CharField(max_length=20, widget=forms.PasswordInput, label=u'Senha')
+    confirm_passwd = forms.CharField(max_length=20, widget=forms.PasswordInput, label=u'Confirmação de senha')
 
-	def clean_nusp(self):
-		nusp = self.cleaned_data['nusp']
-		for char in nusp:
-			if char not in [str(i) for i in range(0, 10)]:
-				raise forms.ValidationError("NUSP inválido.")
-		return nusp
+    def clean_nusp(self):
+        nusp = self.cleaned_data['nusp']
+        for char in nusp:
+            if char not in [str(i) for i in range(0, 10)]:
+                raise forms.ValidationError("NUSP inválido.")
+        return nusp
 
-	def clean(self):
-		cleaned_data = self.cleaned_data
-		passwd = cleaned_data['passwd']
-		confirm_passwd = cleaned_data['confirm_passwd']
-		if passwd != confirm_passwd:
-			self._errors['confirm_passwd'] = self.error_class(["Confirmação de senha inválida."])
-			del cleaned_data['confirm_passwd']
-		return cleaned_data
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        passwd = cleaned_data.get('passwd', '')
+        confirm_passwd = cleaned_data.get('confirm_passwd', '')
+        if passwd and passwd != confirm_passwd:
+            self._errors['confirm_passwd'] = self.error_class(["Confirmação de senha inválida."])
+            del cleaned_data['confirm_passwd']
+        return cleaned_data
 
 
 class LoginForm(forms.Form):
 
-	username = forms.CharField(max_length=20, label='Nome de usuario')
-	passwd = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    username = forms.CharField(max_length=20, label=u'Nome de usuario')
+    passwd = forms.CharField(max_length=20, widget=forms.PasswordInput, label=u'Senha')
 
 class GetCodeForm(forms.Form):
     code = forms.CharField(max_length=1000, label='Código', widget=forms.Textarea(attrs={'class':'special'}) )
     test = forms.CharField(max_length=1000, label='Teste', widget=forms.Textarea(attrs={'class':'special'}) )
 
-    
+
--- ListaOnlineXP/course/views.py
+++ ListaOnlineXP/course/views.py
@@ -12,64 +12,75 @@ import os.path
 from subprocess import Popen, PIPE
 import shlex
 
+def get_student(request):
+	if request.user.is_authenticated():
+		try:
+			student = Student.objects.get(user=request.user)
+			if student is not None:
+				return student
+		except:
+			return None
+	return None
+
 def home(request):
-    if request.user.is_authenticated():
-        try:
-            student = Student.objects.get(user=request.user)
-            if student is not None:
-                return HttpResponseRedirect('/course')
-        except:
-            return render_to_response('home.html')
-    return render_to_response('home.html')
+	if get_student(request) is not None:
+		return HttpResponseRedirect('/course')
+	return HttpResponseRedirect('/login')
+
 
 def signup(request):
-    values = {}
-    values.update(csrf(request))
-    if request.method == 'GET':
-        form = SignUpForm()
-    else:
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            username = form.cleaned_data['username']
-            nusp = form.cleaned_data['nusp']
-            passwd = form.cleaned_data['passwd']
-            try:
-                user = User.objects.create_user(username, '', passwd)
-                student = Student(name=name, nusp=nusp, user=user)
-                student.save()
-                return HttpResponseRedirect('/login/')
-            except:
-                form._errors["username"] = form.error_class(["Usuário já cadastrado."])
-    values['form'] = form
-    return render_to_response('signup.html', values)
+	if get_student(request) is not None:
+		return HttpResponseRedirect('/')
+	values = {}
+	values.update(csrf(request))
+	if request.method == 'GET':
+		form = SignUpForm()
+	else:
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['name']
+			username = form.cleaned_data['username']
+			nusp = form.cleaned_data['nusp']
+			passwd = form.cleaned_data['passwd']
+			try:
+				user = User.objects.create_user(username, '', passwd)
+				student = Student(name=name, nusp=nusp, user=user)
+				student.save()
+				return HttpResponseRedirect('/login')
+			except:
+				form._errors["username"] = form.error_class(["Usuário já cadastrado."])
+	values['form'] = form
+	return render_to_response('signup.html', values)
 
 def login(request):
-    values = {}
-    values.update(csrf(request))
-    if request.method == 'GET':
-        form = LoginForm()
-    else:
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            passwd = form.cleaned_data['passwd']
-            try:
-                user = User.objects.get(username=username)
-                if user.check_password(passwd):
-                    user = auth.authenticate(username=username, password=passwd)
-                    auth.login(request, user)
-                    return HttpResponseRedirect('/course')
-                else:
-                    form._errors['passwd'] = form.error_class(['Senha incorreta.'])
-            except:
-                form._errors['username'] = form.error_class(['Usuário não cadastrado.'])
-    values['form'] = form
-    return render_to_response('login.html', values)
+	if get_student(request) is not None:
+		return HttpResponseRedirect('/')
+	values = {}
+	values.update(csrf(request))
+	if request.method == 'GET':
+		form = LoginForm()
+	else:
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			passwd = form.cleaned_data['passwd']
+			try:
+				user = User.objects.get(username=username)
+				if user.check_password(passwd):
+					user = auth.authenticate(username=username, password=passwd)
+					auth.login(request, user)
+					return HttpResponseRedirect('/course')
+				else:
+					form._errors['passwd'] = form.error_class(['Senha incorreta.'])
+			except:
+				form._errors['username'] = form.error_class(['Usuário não cadastrado.'])
+	values['form'] = form
+	return render_to_response('login.html', values)
 
 def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect('/')
+	if get_student(request) is not None:
+		auth.logout(request)
+	return HttpResponseRedirect('/')
 
 def course(request, course_id):
 	values = {}
@@ -77,79 +88,72 @@ def course(request, course_id):
 	try:
 		course = Course.objects.get(id=int(course_id))
 	except:
-		return HttpResponse("Disciplina não existente.")
+		raise Http404
 	if course is not None:
-		if request.user.is_authenticated():
-			student = Student.objects.get(user=request.user)
-			if student is not None:
-				if course in student.courses.all():
-					subscribe = False
-				else:
-					subscribe = True
-				values['subscribe'] = subscribe
-				values['course'] = course
-				if request.method == 'POST':
-					student.courses.add(course)
-					student.save()
-					return HttpResponseRedirect('/')
-				else:
-					return render_to_response('course.html', values)
+		student = get_student(request)
+		if student is not None:
+			if course in student.courses.all():
+				subscribe = False
 			else:
-				return HttpResponse("Usuário não é um estudante.")
+				subscribe = True
+			values['subscribe'] = subscribe
+			values['course'] = course
+			if request.method == 'POST':
+				student.courses.add(course)
+				student.save()
+				return HttpResponseRedirect('/')
+			else:
+				return render_to_response('course.html', values)
 		else:
-			return HttpResponseRedirect('/login/')
-	return HttpResponse("Disciplina não existente.")
+			return HttpResponseRedirect('/login')
+	raise Http404
 
 def course_list(request):
-    if request.user.is_authenticated():
-        try: 
-            student = Student.objects.get(user=request.user)
-            if student is not None:
-                course_list = list(Course.objects.all())
-                return render_to_response('course_list.html', {'course_list':  course_list, 'student': student})
-        except:
-            return HttpResponseRedirect('/')
-    return HttpResponseRedirect('/')
+	if get_student(request) is None:
+		return HttpResponseRedirect('/')
+	student = Student.objects.get(user=request.user)
+	course_list = list(Course.objects.all())
+	return render_to_response('course_list.html', {'course_list':  course_list, 'student': student})
 
 def get_code(request):
-    values = {}
-    values.update(csrf(request))
-    if request.method == 'GET':
-        form = GetCodeForm()
-    else:
-        form = GetCodeForm(request.POST)
-        if form.is_valid():
+	values = {}
+	values.update(csrf(request))
+	if request.method == 'GET':
+		form = GetCodeForm()
+	else:
+		form = GetCodeForm(request.POST)
+		if form.is_valid():
 
-            code_file = open("/tmp/Code.java", 'w')
-            code_file.write(request.POST['code'])
-            code_file.close()
+			code_file = open("/tmp/Code.java", 'w')
+			code_file.write(request.POST['code'])
+			code_file.close()
 
-            test_file = open("/tmp/TestCode.java", 'w')
-            test_file.write(request.POST['test'])
-            test_file.close()
+			test_file = open("/tmp/TestCode.java", 'w')
+			test_file.write(request.POST['test'])
+			test_file.close()
 
-            junit_location = os.path.join(os.path.dirname(__file__), 'junit/junit.jar').replace('\\', '/')
+			junit_location = os.path.join(os.path.dirname(__file__), 'junit/junit.jar').replace('\\', '/')
 
-            code_compile_command = "javac -classpath /tmp/:" + junit_location + " /tmp/Code.java"
-            test_compile_command = "javac -classpath /tmp/:" + junit_location + " /tmp/TestCode.java"
-            test_command = "java -classpath /tmp/:" + junit_location + " -Djava.security.manager org.junit.runner.JUnitCore TestCode"
+			code_compile_command = "javac -classpath /tmp/:" + junit_location + " /tmp/Code.java"
+			test_compile_command = "javac -classpath /tmp/:" + junit_location + " /tmp/TestCode.java"
+			test_command = "java -classpath /tmp/:" + junit_location + " -Djava.security.manager org.junit.runner.JUnitCore TestCode"
 
-            code_compile_args = shlex.split(code_compile_command)
-            test_compile_args = shlex.split(test_compile_command)
-            test_args = shlex.split(test_command)
+			code_compile_args = shlex.split(code_compile_command)
+			test_compile_args = shlex.split(test_compile_command)
+			test_args = shlex.split(test_command)
 
-            code_compile = Popen(code_compile_args, stdout=PIPE, stderr=PIPE)
-            code_compile_output = code_compile.stderr.read()
+			code_compile = Popen(code_compile_args, stdout=PIPE, stderr=PIPE)
+			code_compile_output = code_compile.stderr.read()
 
-            test_compile = Popen(test_compile_args, stdout=PIPE, stderr=PIPE)
-            test_compile_output = test_compile.stderr.read()
+			test_compile = Popen(test_compile_args, stdout=PIPE, stderr=PIPE)
+			test_compile_output = test_compile.stderr.read()
 
-            test = Popen(test_args, stdout=PIPE, stderr=PIPE)
-            test_output = test.stdout.read()
+			test = Popen(test_args, stdout=PIPE, stderr=PIPE)
+			test_output = test.stdout.read()
 
-            values["code_compile_output"] = code_compile_output
-            values["test_compile_output"] = test_compile_output
-            values["test_output"] = test_output
+			values["code_compile_output"] = code_compile_output
+			values["test_compile_output"] = test_compile_output
+			values["test_output"] = test_output
 
-    values['form'] = form
-    return render_to_response('get_code.html', values)
+	values['form'] = form
+	return render_to_response('get_code.html', values)
