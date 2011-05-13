0d4c1ee8220439a933d1a32f0663567739970fe6
rika <rika@silver-lotus.local>
2011-05-13 10:52:52 -0300

    removed StudentAdmin from authentication

--- ListaOnlineXP/authentication/admin.py
+++ ListaOnlineXP/authentication/admin.py
@@ -3,8 +3,4 @@
 from django.contrib import admin
 from models import Student
 
-
-class StudentAdmin(admin.ModelAdmin):
-    fields = ('name', 'nusp', 'courses')
-
-admin.site.register(Student, StudentAdmin)
+admin.site.register(Student)
