eaca262b0404728cdd844363aff0381a7c5276ed
Hugo Soares Zaggo <hugozaggo@gmail.com>
2011-06-10 08:49:14 -0300

    Changed the way blank answers are allowed in Multiple Choice Answers: it no longer overrides save(). The form presents a blank alternative, which when selected, sets chosen_alternative to None.

--- ListaOnlineXP/exerciselist/forms.py
+++ ListaOnlineXP/exerciselist/forms.py
@@ -13,7 +13,7 @@ class MultipleChoiceAnswerForm(forms.ModelForm):
     def __init__(self, *args, **kargs):
         super(MultipleChoiceAnswerForm, self).__init__(*args, **kargs)
         self.fields['chosen_alternative'].queryset = self.instance.question_answered.casted().get_alternatives()
-        self.fields['chosen_alternative'].empty_label=None
+        self.fields['chosen_alternative'].empty_label='Em branco'
         self.fields['chosen_alternative'].label='Resposta'
         
 
