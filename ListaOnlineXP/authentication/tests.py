2ed5cb57adac7fb4d642f300ed46f717e312f2e5
William Mizuta <mizuta@ime.usp.br>
2011-06-24 11:35:15 -0300

    _Corrected authentication test

--- ListaOnlineXP/authentication/tests.py
+++ ListaOnlineXP/authentication/tests.py
@@ -12,16 +12,18 @@ class CourseTestCase(TestCase):
         self.user1 = User.objects.create(username=u"koiti", password=u"lalala")
         self.user2 = User.objects.create(username=u"tsp", password=u"weasd")
         self.user3 = User.objects.create(username=u"milan", password=u"1234")
-        self.student1 = Student.objects.create(name=u"Steven Koiti Tsukamoto", nusp=u"6431089", user=User.objects.get(id=1))
-        self.student2 = Student.objects.create(name=u"Thiago da Silva Pinheiro", nusp=u"6797000", user=User.objects.get(id=2))
-        self.student3 = Student.objects.create(name=u"Bruno Milan Perfetto", nusp=u"123456", user=User.objects.get(id=3))
+        self.student1 = Student.objects.create(name=u"Steven Koiti Tsukamoto", nusp=u"6431089", user=User.objects.get(id=2))
+        self.student2 = Student.objects.create(name=u"Thiago da Silva Pinheiro", nusp=u"6797000", user=User.objects.get(id=3))
+        self.student3 = Student.objects.create(name=u"Bruno Milan Perfetto", nusp=u"123456", user=User.objects.get(id=4))
         
     def testStudentDB(self):
 
         self.assertEqual(Student.objects.get(name=u"Steven Koiti Tsukamoto").name, u"Steven Koiti Tsukamoto")
         self.assertEqual(Student.objects.get(name=u"Thiago da Silva Pinheiro").id, 2)
         self.assertEqual(Student.objects.get(name=u"Bruno Milan Perfetto").nusp, u"123456")
+        self.assertEqual(Student.objects.get(name=u"Steven Koiti Tsukamoto").user.username, u"koiti")
         self.assertEqual(Student.objects.get(name=u"Thiago da Silva Pinheiro").user.username, u"tsp")
+        self.assertEqual(Student.objects.get(name=u"Bruno Milan Perfetto").user.username, u"milan")
         self.assertNotEqual(Student.objects.get(id=u"3").name,u"Steven Koiti Tsukamoto")
         self.assertNotEqual(Student.objects.get(id=u"3").name,u"Thiago da Silva Pinheiro")
         self.assertEqual(Student.objects.count(), 3)
