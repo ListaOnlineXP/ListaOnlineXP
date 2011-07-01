e43957d7b63b032bf0e2fe8bc72a320dd89972fc
rika <oda.ric@gmail.com>
2011-07-01 14:42:54 -0300

    Added tools.jar so java correction works in any unix. Added more info in README

--- ListaOnlineXP/exerciselist/java/compilar_classes.sh
+++ ListaOnlineXP/exerciselist/java/compilar_classes.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-javac -encoding utf8 -cp ./ Compilar.java; javac -encoding utf8 -cp ./ StreamGobbler.java; javac -encoding utf8 -cp ./ RuntimeExecutor.java; javac -encoding utf8 -cp ./ StringChanger.java ; javac -encoding utf8 -cp ./ StringConverter.java; javac -encoding utf8 -cp ./ JavaTester.java
+javac -encoding utf8 -cp ./ -classpath ./tools.jar Compilar.java
+javac -encoding utf8 -cp ./ StreamGobbler.java
+javac -encoding utf8 -cp ./ RuntimeExecutor.java
+javac -encoding utf8 -cp ./ StringChanger.java
+javac -encoding utf8 -cp ./ StringConverter.java
+javac -encoding utf8 -cp ./ JavaTester.java
--- ListaOnlineXP/exerciselist/java/compilar_classes_debian.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-javac -encoding utf8 -cp ./ -classpath /usr/lib/jvm/java-6-sun/lib/tools.jar Compilar.java;
-javac -encoding utf8 -cp ./ StreamGobbler.java
-javac -encoding utf8 -cp ./ RuntimeExecutor.java
-javac -encoding utf8 -cp ./ StringChanger.java
-javac -encoding utf8 -cp ./ StringConverter.java
-javac -encoding utf8 -cp ./ JavaTester.java
